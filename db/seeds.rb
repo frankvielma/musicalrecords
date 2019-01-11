95f93e0062078df9658610a82230356508effb2e
frankvielma <frankvielma@gmail.com>
2019-01-11 10:34:01 -0400

    add initial example data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.create(name: 'Artist 1', description: 'Description 1')
+Artist.create(name: 'Artist 2', description: 'Description 2')
+Artist.create(name: 'Artist 3', description: 'Description 3')
+Artist.create(name: 'Artist 4', description: 'Description 4')
+Artist.create(name: 'Artist 5', description: 'Description 5')
